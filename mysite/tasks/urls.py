2cfe3393800578cdb478608d652be196f8ccde00
Eve Wright <e_wright5@mail.fhsu.edu>
2024-04-28 19:22:22 -0500

    Added all views, templates, and urls for tasks

--- mysite/tasks/urls.py
+++ mysite/tasks/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
 from . import views
+from .views import TasksIndexView
 
+
+app_name = 'tasks'
 urlpatterns = [
-    path("", views.index, name="index")
+    path('detail/<int:pk>/', views.TasksDetailView.as_view(), name='detail'),
+    path("", views.TasksIndexView.as_view(), name="index"),
+    path("create", views.TasksCreateView.as_view(), name="create"),
+    path("update/<int:pk>/", views.TasksUpdateView.as_view(), name="update"),
+    path("task-delete/<int:pk>/", views.TasksDeleteView.as_view(), name="task-delete")
     ]
--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from .forms import TasksForm
+from .models import Task
+from django.urls import reverse, reverse_lazy
 
 
-def index(request):
-    return HttpResponse("This is the main index")
+class TasksIndexView(generic.ListView):
+    model = Task
+    template_name = "tasks/index.html"
+    context_object_name = "tasks"
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TasksDetailView(generic.DetailView):
+    model = Task
+    template_name = "tasks/detail.html"
+    context_object_name = "task"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.author != self.request.user:
+            return redirect('tasks:index')
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TasksCreateView(generic.CreateView):
+    model = Task
+    form_class = TasksForm
+    template_name = "tasks/create.html"
+
+    success_url = reverse_lazy("tasks:index")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class TasksUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TasksForm
+    template_name = "tasks/update.html"
+    success_url = reverse_lazy("tasks:index")
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.author != self.request.user:
+            return redirect('tasks:index')
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TasksDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "tasks/index.html"
+    success_url = reverse_lazy("tasks:index")
