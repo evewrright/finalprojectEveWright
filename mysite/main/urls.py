34af767dfe33aad71fab47ff832db896bab60f12
Eve Wright <e_wright5@mail.fhsu.edu>
2024-04-29 20:26:24 -0500

    Added login view and url paths for login and logout.

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
+app_name = 'main'
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('login/', views.MyLoginView.as_view(), name='my-login'),
+    path('logout/', LogoutView.as_view(next_page='main:my-login'), name='logout')
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views import generic
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def index(request):
     return HttpResponse("This is the main index")
+
+
+class MyLoginView(LoginView):
+    template_name = 'main/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("tasks:index")
