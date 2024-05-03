9c07ff0d0dbdc86f4dfae9a22daaad55348eee66
Eve Wright <e_wright5@mail.fhsu.edu>
2024-05-03 00:08:06 -0500

    Added view and corresponding url path for chatgpt API request

--- mysite/appts/urls.py
+++ mysite/appts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.ApptsIndexView.as_view(), name="index"),
     path('detail/<int:pk>/', views.ApptsDetailView.as_view(), name='detail'),
     path("create", views.ApptsCreateView.as_view(), name="create"),
-    path("update/<int:pk>/", views.ApptsUpdateView.as_view(), name="update")
+    path("update/<int:pk>/", views.ApptsUpdateView.as_view(), name="update"),
+    path('generate-paragraph/', views.generate_paragraph, name='generate-paragraph')
     ]
--- mysite/appts/views.py
+++ mysite/appts/views.py
@@ -5,12 +5,23 @@ from django.views import generic
 from .models import Appointment
 from django.urls import reverse, reverse_lazy
 from .forms import ApptsForm
-
+import requests
+from django.http import JsonResponse
 
 class ApptsIndexView(generic.ListView):
     model = Appointment
     template_name = "appts/index.html"
     context_object_name = "appointments"
+
+    def get_queryset(self):
+        # Only retrieve appts that were created by current user
+        if self.request.user.is_authenticated:
+            return Appointment.objects.filter(author_id=self.request.user)
+        else:
+            return Appointment.objects.none()
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class ApptsDetailView(generic.DetailView):
@@ -29,7 +40,6 @@ class ApptsCreateView(generic.CreateView):
     model = Appointment
     form_class = ApptsForm
     template_name = "appts/create.html"
-
     success_url = reverse_lazy("appts:index")
 
     def form_valid(self, form):
@@ -54,3 +64,25 @@ class ApptsDeleteView(generic.DeleteView):
     model = Appointment
     template_name = "appts/index.html"
     success_url = reverse_lazy("appts:index")
+
+
+def generate_paragraph(request):
+    if request.method == 'POST':
+        input_text = request.POST.get('inputTextArea')
+        url = "https://chatgpt-api8.p.rapidapi.com/"
+        headers = {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'b3d9b4f2aamsh2400155d584eb85p14f800jsna29b2bbdc421',
+            'X-RapidAPI-Host': 'chatgpt-api8.p.rapidapi.com'
+        }
+        payload = [
+            {
+                "content": 'turn these notes about a student into a paragraph with all complete sentences' + input_text,
+                "role": "user"
+            }
+        ]
+        response = requests.post(url, json=payload, headers=headers)
+        if response.status_code == 200:
+            result = response.json()
+            my_paragraph = result['text']
+            return my_paragraph
